e924c521c96af89254f5adf5b92238b0fb155add
Lin <linchen7@foxmail.com>
2016-03-11 22:14:22 +0800

    Support for jsp files

--- recommendation/src/main/java/edu/nju/paperCiteAnalysis/recommendation/Main.java
+++ recommendation/src/main/java/edu/nju/paperCiteAnalysis/recommendation/Main.java
@@ -1,13 +1,12 @@
 package edu.nju.paperCiteAnalysis.recommendation;
 
-import edu.nju.paperCiteAnalysis.recommendation.view.HelloJetty;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.glassfish.jersey.servlet.ServletContainer;
+import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
  * Created by hazel on 2016-03-11.
+ * Main
  */
 public class Main {
 
@@ -16,21 +15,32 @@ public class Main {
     }
 
     private static void initJettyServer() {
-        ServletHolder servletHolder = new ServletHolder(ServletContainer.class);
-        servletHolder.setInitParameter("jersey.config.server.provider.classnames",
-                HelloJetty.class.getCanonicalName());
-
         Server jettyServer = new Server(8080);
-        ServletContextHandler context =
-                new ServletContextHandler(jettyServer, "/", ServletContextHandler.SESSIONS);
-        context.addServlet(servletHolder, "/*");
+        WebAppContext ctx = new WebAppContext();
+        ctx.setDescriptor(Main.class.getResource("/webapp/WEB-INF/web.xml").getPath());
+        ctx.setResourceBase(Main.class.getResource("/webapp").getPath());
+        ctx.setContextPath("/");
 
+        //Including the JSTL jars for the webapp.
+        ctx.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
+                ".*/[^/]*jstl.*\\.jar$");
+
+        //Enabling the Annotation based configuration
+        Configuration.ClassList classList =
+                Configuration.ClassList.setServerDefault(jettyServer);
+        classList.addAfter("org.eclipse.jetty.webapp.FragmentConfiguration",
+                "org.eclipse.jetty.plus.webapp.EnvConfiguration",
+                "org.eclipse.jetty.plus.webapp.PlusConfiguration");
+        classList.addBefore("org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
+                "org.eclipse.jetty.annotations.AnnotationConfiguration");
+
+        jettyServer.setHandler(ctx);
         try {
             jettyServer.start();
             jettyServer.join();
             System.out.println("Jetty server started.");
         } catch (Exception e) {
-            System.out.println("Errors occurred when start jetty server:  " + e.getMessage());
+            System.err.println("Errors occurred when start jetty server:  " + e.getMessage());
             System.exit(1);
         } finally {
             jettyServer.destroy();
