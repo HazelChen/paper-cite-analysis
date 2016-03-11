f37dd716f95204e16678617c9590b2c1c962215d
Lin <linchen7@foxmail.com>
2016-03-11 22:56:28 +0800

    Add recommendation front and remove hello jetty

---
+++ recommendation/src/main/java/edu/nju/paperCiteAnalysis/recommendation/front/RecommendationFront.java
@@ -0,0 +1,12 @@
+package edu.nju.paperCiteAnalysis.recommendation.front;
+
+/**
+ * Created by hazel on 2016-03-11.
+ * Before recommendation
+ */
+public class RecommendationFront {
+    //trash
+
+
+
+}
