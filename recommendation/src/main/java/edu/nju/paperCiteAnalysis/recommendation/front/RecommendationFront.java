fc8a54d51d461e4e97254d7f4c6132f07e58e1a5
Lin <linchen7@foxmail.com>
2016-03-11 22:58:30 +0800

    Add trash in Front

--- recommendation/src/main/java/edu/nju/paperCiteAnalysis/recommendation/front/RecommendationFront.java
+++ recommendation/src/main/java/edu/nju/paperCiteAnalysis/recommendation/front/RecommendationFront.java
@@ -1,11 +1,19 @@
 package edu.nju.paperCiteAnalysis.recommendation.front;
 
+import edu.nju.paperCiteAnalysis.recommendation.common.Bibtex;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by hazel on 2016-03-11.
  * Before recommendation
  */
 public class RecommendationFront {
-    //trash
+    private Map<List<Bibtex>, Set<Bibtex>> trash;
+
+
 
 
 
